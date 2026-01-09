698df62dd1c30d53df6109b6e566f8012c61139d
gb69prof <gb69prof@users.noreply.github.com>
2026-01-09 09:03:49 +0100

    Delete Prova1/assets/app.js

--- Prova1/assets/app.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('.cardLink').forEach(link=>{
-  link.addEventListener('touchstart', ()=>{ link.classList.add('is-touch'); }, {passive:true});
-  link.addEventListener('touchend', ()=>{ link.classList.remove('is-touch'); }, {passive:true});
-});
